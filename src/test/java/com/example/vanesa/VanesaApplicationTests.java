0be424cef69266a5dfc8f271085e1aae545cf638
vanesaarandavera <vanesaa715@gmail.com>
2023-09-15 22:40:27 -0300

    subiendo api empresa Pied Piper

---
+++ src/test/java/com/example/vanesa/VanesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vanesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
